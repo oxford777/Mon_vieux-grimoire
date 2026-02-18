1687e32b1c9247f43043b5caf8ab6b654282e315
Frederique PUJOL <frederique.pujol2024@campus-eni.fr>
2026-02-18 19:14:47 +0100

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log('Connexion à MongoDB échouée !', error));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
